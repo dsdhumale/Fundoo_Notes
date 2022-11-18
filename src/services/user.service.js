a9af7c1638af3c27a2ffec81981c863b1e1c1e65
dsdhumale <d.sdhumale@gmail.com>
2022-11-18 22:38:29 +0530

    Refactor: By using bcrypt hashing password done while user registration

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,23 +1,29 @@
 import User from '../models/user.model';
+import bcrypt from 'bcrypt';
 
-//create new user
+
+//Register and create new user and hashing password
 export const newUserRegistration = async (body) => {
-  const data = await User.find({emailID: body.emailID});
-  if (data.length != 0) {
+  const email = await User.find({ emailID: body.emailID });
+  if (email.length != 0) {
     throw new Error("Already exist,Enter another")
   } else {
+    const saltRounds = 10;
+    const hashpassword = await bcrypt.hash(body.password, saltRounds);
+    body.password = hashpassword;
     const data = await User.create(body);
-  return data;
-  
-}
+    return data;
+  }
 };
+
+
 
 //Login with email ID and password
 export const login = async (body) => {
-  const data = await User.find({emailID: body.emailID});
+  const data = await User.find({ emailID: body.emailID });
   if (data.length !== 0) {
-    const data = await User.find({password: body.password});
-    if(data.length != 0){
+    const data = await User.find({ password: body.password });
+    if (data.length != 0) {
       return data;
     }
     else {
@@ -29,6 +35,5 @@ export const login = async (body) => {
 };
 
 
-    
-    
-  
+
+
