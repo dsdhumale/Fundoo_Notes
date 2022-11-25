e2cfc8a25ecfd07d5f85d5734550bb32e13cd54f
dsdhumale <d.sdhumale@gmail.com>
2022-11-25 13:19:00 +0530

    Added : Ability to trash particular note by ID only for authenticated user

--- src/services/note.service.js
+++ src/services/note.service.js
@@ -8,83 +8,63 @@ export const createNote = async (body) => {
 
 //get all notes
 export const getAllNotes = async (userID) => {
-    const data = await Note.find({userID});
-    if (data.length != 0) {
-      return data;
-    }
-    else {
-      throw new Error("Notes are not created for this user");
-    }
-  };
+  const data = await Note.find({ userID });
+  if (data.length != 0) {
+    return data;
+  }
+  else {
+    throw new Error("Notes are not created for this user");
+  }
+};
 
 //get note by _id
-export const getNoteByID = async (_id,userID) => {
-    const data = await Note.findOne({_id,userID});
-    if (data!== null) {
-      return data;
-    } else {
-      throw new Error("Incorrect note ID");
-    }
-  };
-
-  //update note by _id
-export const updateNoteByID = async (_id,userID, body) => {
-    const data = await Note.findOneAndUpdate(
-      {
-        _id,
-        userID
-      },
-      body,
-      {
-        new: true
-      }
-    );
-    if (data!== null) {
-      return data;
-    } else {
-      throw new Error("Incorrect note ID");
-    }
-  };
-
-  //delete note by _id
-export const deleteNoteByID = async (_id,userID) => {
-    const data = await Note.findOneAndDelete({_id,userID});
-    if (data!== null) {
-      return data;
-    } else {
-      throw new Error("Incorrect note ID");
-    }
-  };
-  
-  //Archive note by _id
-
-export const isArchived = async (_id,userID) => {
-    const data = await Note.findByIdAndUpdate(
-      {
-        _id,
-        userID
-      },
-      {isArchived: true},
-      {
-        new: true
-      }
-    );
+export const getNoteByID = async (_id, userID) => {
+  const data = await Note.findOne({ _id, userID });
+  if (data !== null) {
     return data;
-  };
+  } else {
+    throw new Error("Incorrect note ID");
+  }
+};
 
-  //Trash note by _id
-
-export const isTrash = async (_id,userID) => {
-    const data = await Note.findByIdAndUpdate(
-      {
-        _id,
-        userID
-      },
-      {isTrash: true},
-      {
-        new: true
-      }
-    );
+//update note by _id
+export const updateNoteByID = async (_id, userID, body) => {
+  const data = await Note.findOneAndUpdate( { _id, userID }, body, { new: true } );
+  if (data !== null) {
     return data;
-  };
-  
+  } else {
+    throw new Error("Incorrect note ID");
+  }
+};
+
+//delete note by _id
+export const deleteNoteByID = async (_id, userID) => {
+  const data = await Note.findOneAndDelete({ _id, userID });
+  if (data !== null) {
+    return data;
+  } else {
+    throw new Error("Incorrect note ID");
+  }
+};
+
+//Archive note by _id
+
+export const isArchived = async (_id, userID) => {
+  const data = await Note.findByIdAndUpdate({_id,userID},{ isArchived: true },{new: true});
+  if (data !== null) {
+    return data;
+  } else {
+    throw new Error("Incorrect note ID");
+  }
+};
+
+//Trash note by _id
+
+export const isTrash = async (_id, userID) => {
+  const data = await Note.findByIdAndUpdate({ _id,userID },{ isTrash: true },{ new: true });
+  if (data !== null) {
+    return data;
+  } else {
+    throw new Error("Incorrect note ID");
+  }
+};
