71b2260137e7c3b0d72212a53916a9f4baee8be7
dsdhumale <d.sdhumale@gmail.com>
2022-11-29 23:58:21 +0530

    [Devendra]Added : Test cases for user registration and login

--- tests/integration/user.test.js
+++ tests/integration/user.test.js
@@ -8,7 +8,7 @@ describe('User APIs Test', () => {
   before((done) => {
     const clearCollections = () => {
       for (const collection in mongoose.connection.collections) {
-        mongoose.connection.collections[collection].deleteOne(() => {});
+        mongoose.connection.collections[collection].deleteOne(() => { });
       }
     };
 
@@ -26,16 +26,154 @@ describe('User APIs Test', () => {
     done();
   });
 
-  describe('GET /users', () => {
-    it('should return empty array', (done) => {
-      request(app)
-        .get('/api/v1/users')
-        .end((err, res) => {
-          expect(res.statusCode).to.be.equal(200);
-          expect(res.body.data).to.be.an('array');
+  // 1 - Test case for user registration
+  describe('POST / UserRegistration', () => {
+    it('1.Create user and should return status 201', (done) => {
 
+      const userDetails = {
+        "firstName": "Aniket",
+        "lastName": "Mankumare",
+        "emailID": "aniketdd34@gmail.com",
+        "password": "Aniket123"
+      }
+
+      request(app)
+        .post('/api/v1/users/registration')
+        .send(userDetails)
+        .end((err, res) => {
+          expect(res.statusCode).to.be.equal(201);
           done();
         });
     });
+    // 2- Test case for invalid first name
+    it('2.While creating user due to invalid first name should return status 500', (done) => {
+
+      const userDetails = {
+        "firstName": "A",
+        "lastName": "Mankumare",
+        "emailID": "aniket3432@gmail.com",
+        "password": "Aniket123"
+      }
+
+      request(app)
+        .post('/api/v1/users/registration')
+        .send(userDetails)
+        .end((err, res) => {
+          expect(res.statusCode).to.be.equal(500);
+          done();
+        });
+    });
+    // 3- Test case for invalid last name
+    it('3.While creating user due to invalid last name should return status 500', (done) => {
+
+      const userDetails = {
+        "firstName": "Aniket",
+        "lastName": "M",
+        "emailID": "aniket34@gmail.com",
+        "password": "Aniket123"
+      }
+
+      request(app)
+        .post('/api/v1/users/registration')
+        .send(userDetails)
+        .end((err, res) => {
+          expect(res.statusCode).to.be.equal(500);
+          done();
+        });
+    });
+    // 4- Test case for invalid email-id
+    it('4.While creating user due to invalid email should return status 500', (done) => {
+
+      const userDetails = {
+        "firstName": "Aniket",
+        "lastName": "Mankumare",
+        "emailID": "aniketgmail.com",
+        "password": "Aniket123"
+      }
+
+      request(app)
+        .post('/api/v1/users/registration')
+        .send(userDetails)
+        .end((err, res) => {
+          expect(res.statusCode).to.be.equal(500);
+          done();
+        });
+    });
+    // 5- Test case for invalid password
+    it('5.While creating user due to invalid password should return status 500', (done) => {
+
+      const userDetails = {
+        "firstName": "Aniket",
+        "lastName": "Mankumare",
+        "emailID": "aniket345678@gmail.com",
+        "password": "A"
+      }
+
+      request(app)
+        .post('/api/v1/users/registration')
+        .send(userDetails)
+        .end((err, res) => {
+          expect(res.statusCode).to.be.equal(500);
+          done();
+        });
+    });
+
   });
 });
+var token;
+// 6 - Test case for user login
+describe('POST/UserLogin', () => {
+  it('6.Given user details in login should get logged into account', (done) => {
+  const userDetails = {
+
+    "emailID": "aniketdd34@gmail.com",
+    "password": "Aniket123"
+  }
+    request(app)
+      .post('/api/v1/users/login')
+      .send(userDetails)
+      .end((err, res) => {
+        token = res.body.data
+        expect(res.statusCode).to.be.equal(202);
+        done();
+      });
+  });
+});
+
+
+// 7 - Test case for invalid user email id
+describe('POST/UserLogin', () => {
+  it('7.While log in due to invalid email id should return status 500', (done) => {
+  const userDetails = {
+
+    "emailID": "anike@gmail.com",
+    "password": "Aniket123"
+  }
+    request(app)
+      .post('/api/v1/users/login')
+      .send(userDetails)
+      .end((err, res) => {
+        expect(res.statusCode).to.be.equal(500);
+        done();
+      });
+  });
+});
+
+// 8 - Test case for invalid user password
+describe('POST/UserLogin', () => {
+  it('8.While log in due to invalid password should return status 500', (done) => {
+  const userDetails = {
+
+    "emailID": "aniketdd34@gmail.com",
+    "password": "Aniket12"
+  }
+    request(app)
+      .post('/api/v1/users/login')
+      .send(userDetails)
+      .end((err, res) => {
+        expect(res.statusCode).to.be.equal(500);
+        done();
+      });
+  });
+});
+
